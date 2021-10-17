aa201197b46aca20e58b64e99c1068735ce41862
Anthonywambui <92174483+Anthonywambui@users.noreply.github.com>
2021-10-16 22:04:59 -0700

    Create chege.c
    a thousand miles journey starts with a single step..

---
+++ chege.c
@@ -0,0 +1,18 @@
+[11:41 AM, 10/16/2021] Mr Robbinson: #include<stdio.h>
+/arithmetic operators/
+int main()
+{
+	int r;
+	float area, volume, pi;
+	pi=3.142;
+	printf("enter the radius");//prompt the user to enter the value of the radius
+	scanf("%d", &r);
+	printf("\n area of sphere=4*pi*r*r");
+	area=4*pi*r*r;
+	printf("volume of the sphere");
+	volume=1.333*pi*r*r;
+	printf("\narea=%f", area);
+	printf("\nvolume=%f", volume);
+	printf("\narea=%f", area);
+  return 0;
+}
